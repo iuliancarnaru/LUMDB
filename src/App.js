7df46af226988154823bee7e5b3aff41c646039a
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-05 16:51:50 +0000

    state in React and toggle visibility example

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    visibility: false
+  }
+
+  toggleVisibility = () => {
+    this.setState(() => ({ visibility: !this.state.visibility }));
+  }
+
   render() {
+    //deconstructing this.state object to grab visibility property from
+    const { visibility } = this.state;
+
     return (
-      <Welcome text="Welcome form React 16"/>
+      <div>
+        <Welcome text="Welcome form React 16"/>
+        
+        <button onClick={this.toggleVisibility}>{visibility ? 'Hide paragraph' : 'Show paragraph'}</button>
+        {visibility && <p>This should show and hide</p>}
+      </div>
     );
   }
 }
 
 class Welcome extends Component {
   render(){
-    
+
     //deconstructing this.props object to grab text property from
     const { text } = this.props;
 
