a398e549497c0a720ea53d2aac4a286aedd58721
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-05 18:35:55 +0000

    static, defautprops & PropTypes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Movie from './Movie';
 const movies = [
   {
     id: 1,
-    title: 'Star Wars'
+    title: 'Star Wars',
+    description: 'Star Wars is an American epic space opera franchise, created by George Lucas and centered around a film series that began with the eponymous 1977 movie. '
   }, {
     id: 2,
     title: 'Spider Man'
@@ -30,7 +31,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
 
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {movies.map(movie => <Movie key={movie.id} movie={movie} description={movie.description}/>)}
 
       </div>
     );
