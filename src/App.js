9357bc31424e7b778de3111e13fd4e7a0572b894
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-05 17:23:30 +0000

    ref's in React

--- src/App.js
+++ src/App.js
@@ -2,37 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    console.log('constructor');
-  }
 
-  componentWillMount() {
-    console.log('will mount');
-  }
-
-  componentDidMount() {
-    console.log('mounted');
-  }
-
-  state = {
-    visibility: false
-  }
-
-  toggleVisibility = () => {
-    this.setState(() => ({ visibility: !this.state.visibility }));
+  submit = () => {
+    console.log(this.text.value);
   }
 
   render() {
-    //deconstructing this.state object to grab visibility property from
-    const { visibility } = this.state;
-
     return (
       <div>
         <Welcome text="Welcome form React 16"/>
-        
-        <button onClick={this.toggleVisibility}>{visibility ? 'Hide paragraph' : 'Show paragraph'}</button>
-        {visibility && <p>This should show and hide</p>}
+        <input type="text" ref={(input) => this.text = input}/>
+        <button onClick={this.submit}>Show value</button>
       </div>
     );
   }
@@ -40,8 +20,6 @@ class App extends Component {
 
 class Welcome extends Component {
   render(){
-
-    //deconstructing this.props object to grab text property from
     const { text } = this.props;
 
     return(
