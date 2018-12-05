aa61aadd5994c1120ab86fda9f506ff04a9d007e
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-05 16:34:40 +0000

    props in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Welcome text="Welcome form React 16"/>
+    );
+  }
+}
+
+class Welcome extends Component {
+  render(){
+    
+    //deconstructing this.props object to grab text property from
+    const { text } = this.props;
+
+    return(
+      <h1>{text}</h1>
     );
   }
 }
