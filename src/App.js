f35100412e92bb6a773513e4a6495bdc77bab32e
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-05 18:17:34 +0000

    iterating and child components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
+
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+  }, {
+    id: 3,
+    title: 'Fantasctic 4'
+  }, {
+    id: 4,
+    title: 'Black Star'
+  }
+];
+
 
 class App extends Component {
-
-  state = {
-    input: ''
-  }
-
-  submit = () => {
-    console.log(this.text.value);
-  }
-
-  updateInput = (event) => {
-    const input = event.target.value.trim();
-    this.setState(() => ({ input }));
-  }
-
   render() {
     return (
-      <div>
-        <Welcome text="Welcome form React 16"/>
-        <h3>{this.state.input}</h3>
-        <input type="text" onChange={this.updateInput} value={this.state.input}/>
-        <input type="text" ref={(input) => this.text = input}/>
-        <button onClick={this.submit}>Show value</button>
+      <div className="App">
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+
       </div>
     );
   }
 }
 
-class Welcome extends Component {
-  render(){
-    const { text } = this.props;
-
-    return(
-      <h1>{text}</h1>
-    );
-  }
-}
 
 export default App;
