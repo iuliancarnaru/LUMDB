158422c1a3e37ce8938bc518e1ffbbe240ceeaee
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-05 17:46:59 +0000

    controled inputs vs uncontroled inputs

--- src/App.js
+++ src/App.js
@@ -3,14 +3,25 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    input: ''
+  }
+
   submit = () => {
     console.log(this.text.value);
+  }
+
+  updateInput = (event) => {
+    const input = event.target.value.trim();
+    this.setState(() => ({ input }));
   }
 
   render() {
     return (
       <div>
         <Welcome text="Welcome form React 16"/>
+        <h3>{this.state.input}</h3>
+        <input type="text" onChange={this.updateInput} value={this.state.input}/>
         <input type="text" ref={(input) => this.text = input}/>
         <button onClick={this.submit}>Show value</button>
       </div>
