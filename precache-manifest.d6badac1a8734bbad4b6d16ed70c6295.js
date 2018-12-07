c9d65adbcd06673d46902c77d49a6feb1a231711
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2018-12-07 17:34:58 +0000

    Updates

---
+++ precache-manifest.d6badac1a8734bbad4b6d16ed70c6295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f81bcb3a1415924927f6",
+    "url": "/myapp/static/css/main.cf93c792.chunk.css"
+  },
+  {
+    "revision": "f81bcb3a1415924927f6",
+    "url": "/myapp/static/js/main.f81bcb3a.chunk.js"
+  },
+  {
+    "revision": "a2702fce8b38fd4e1459",
+    "url": "/myapp/static/js/1.a2702fce.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "ff9a163feb64cd25f5eb42dfece411a4",
+    "url": "/myapp/static/media/logo.ff9a163f.svg"
+  },
+  {
+    "revision": "2171114a53d6a8a63e9475f77190a740",
+    "url": "/myapp/index.html"
+  }
+];
